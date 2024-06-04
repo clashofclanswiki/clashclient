63bc34538907ca31234fd123e5120261141a97f0
Adrian <dmitrukadrian@gmail.com>
2024-06-04 23:45:29 +0300

    fix: domain dev

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks']
   },
-};
+  env: {
+    SERVER_URL: process.env.SERVER_URL,
+    CLIENT_DOMAIN: process.env.CLIENT_DOMAIN
+  }
+}
 
-export default nextConfig;
+export default nextConfig
