8ea94d24b231b95efdf9bccf3e9f412ac03fda64
Adrian <dmitrukadrian@gmail.com>
2024-06-06 10:49:57 +0300

    fix: add next revalidate test news seconds

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -5,31 +5,30 @@ import { CategoryInfo } from '@/components/category-info/category-info'
 
 import { ICategoryOne } from '@/types/category.types'
 
-// async function getCategory() {
-//   const res = await fetch(`http://localhost:4200/api/category/news`, {
-//     next: { revalidate: 3600 }
-//   })
+async function getCategory() {
+  const res = await fetch(`http://localhost:4200/api/category/news`, {
+    next: { revalidate: 3600 }
+  })
 
-//   if (!res.ok) {
-//     throw new Error('Failed to fetch data')
-//   }
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
 
-//   return res.json()
-// }
+  return res.json()
+}
 
 export default async function NewsPage() {
-  // const data: ICategoryOne = await getCategory()
+  const data: ICategoryOne = await getCategory()
 
   return (
     <Flex gap='md' direction='column' align='center'>
-      {/* <CategoryInfo
+      <CategoryInfo
         title={data.data.title}
         description={data.data.description}
       />
       {data.posts.data.map(elem => (
         <CardItem post={elem} key={elem.id} />
-      ))} */}
-      aaa
+      ))}
     </Flex>
   )
 }
